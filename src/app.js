5c74279c642bc999f8ee621fd231459b081e7b32
leonardofmds <leonardo.fmsantos@uniriotec.br>
2020-09-25 23:46:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const { v4: uuid, validate: isUuid } = require('uuid'); 
 
 const app = express();
